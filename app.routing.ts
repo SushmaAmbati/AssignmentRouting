87ac7d920b578e455635f543743a957dcd186983
SushmaAmbati <asushma207@gmail.com>
2018-01-20 20:23:54 -0600

    commited

---
+++ app.routing.ts
@@ -0,0 +1,18 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: 'dashboard',
+    component: DashboardComponent
+  }
+];
+export const routing = RouterModule.forRoot(appRoutes);
